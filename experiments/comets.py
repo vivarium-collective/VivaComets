57e58c6be499d61937e1e67427a0b2bf258ca368
Amin Boroomand <aboroomand@ucmerced.edu>
2024-06-26 13:21:25 -0400

    extracted Visualization functions from run_comets function

--- experiments/comets.py
+++ experiments/comets.py
@@ -5,55 +5,24 @@ COMETS Vivarium Composite Simulation
 import numpy as np
 from processes.diffusion_field import DiffusionField
 from processes.spatial_dfba import SpatialDFBA
-from plots.field import plot_objective_flux, plot_fields_temporal
 from vivarium.core.engine import Engine
 
-# Define comets_config 
+# Configuration
 comets_config = {
-    'total_time': 10,
-    'bounds': [5, 5],
-    'nbins': [2, 2],
+    'total_time':10 ,
+    'time_step': 1,
+    'bounds': [10, 4],
+    'nbins': [10, 4],
+    'molecules': ['glucose', 'acetate', 'Maltose']
 }
 
 # Parameters shared by both processes
 shared_params = {
     'bounds': comets_config['bounds'],
     'nbins': comets_config['nbins'],
-    'molecules': ['glucose', 'oxygen'],
+    'molecules': comets_config['molecules']
 }
 
-species_info = [
-    {
-        "name": "Alteromonas",
-        "model": '../data/Alteromonas_Model.xml',
-        "flux_id_map": {
-            "glucose": "EX_cpd00027_e0",
-            "oxygen": "EX_cpd00007_e0"
-        },
-        "kinetic_params": {
-            "glucose": (0.5, 0.0005),
-            "oxygen": (0.3, 0.0005),
-        },
-        "fixed_bounds": {
-            'EX_cpd00149_e0': (-10, 10)
-        }
-    },
-    {
-        "name": "ecoli",
-        "model": '../data/iECW_1372.xml',
-        "flux_id_map": {
-            "glucose": "EX_glc__D_e",
-            "oxygen": "EX_o2_e"
-        },
-        "kinetic_params": {
-            "glucose": (0.4, 0.6),
-            "oxygen": (0.25, 0.6),
-        },
-        "fixed_bounds": {
-            'EX_fe3dhbzs_e': (0, 10)
-        }
-    }
-]
 
 # Specific parameters for Diffusion Field
 diffusion_field_params = {
@@ -61,51 +30,105 @@ diffusion_field_params = {
     'default_diffusion_dt': 0.001,
     'default_diffusion_rate': 2E-5,
     'diffusion': {
-        'glucose': 6.7E-1,
-        'oxygen': 2.0E-2,
+        'glucose': 2.0E-2,
+        'acetate':2.0E-2,
+        'Maltose':2.0E-2,
+        'Thermotoga': 1.0E-2, 
+        'ecoli': 1.0E-2 
     },
     'advection': {
-        'glucose': (0.01, 0.02),
-        'oxygen': (0.01, 0.01),
+        'glucose': (0.02, -0.03),
+        'acetate': (0.02, -0.03),
+        'Maltose': (0.02, -0.03),
+        'Thermotoga': (0.02, -0.02), # advection vector 
+        'ecoli': (0.02, -0.05)
+    },
+    'clamp_edges': {
+        'glucose': 0.5, 
+        'acetate': 0.5,
+        'Maltose': 0.5,
     }
 }
 
 # Specific parameters for Spatial DFBA
 spatial_dfba_params = {
     **shared_params,
-    'species_info': species_info
-}
-
-initial_state_config = {
-    'random': {
-        'glucose': 200.0,  # Max random value for glucose
-        'oxygen': 200.0,   # Max random value for oxygen
-        'species': {
-            'ecoli': 0.5,   # Max random value for E. coli biomass
-            'Alteromonas': 0.5   # Max random value for Alteromonas biomass
+    'species_info': [
+        {
+            'name': 'Thermotoga',
+            'model': '../data/iLJ478.xml',  # Path to FBA model file 
+            'flux_id_map': {
+                'glucose': 'EX_glc__D_e',
+                'acetate': "EX_ac_e",
+                'Maltose': "EX_malt_e"
+            },
+            'kinetic_params': {
+                'glucose': (0.1, 1),  # Km, Vmax for glucose
+                'Maltose': (0.1, 0.8)
+            },
+            # "fixed_bounds": {
+            #     'EX_cpd00058_e0': {'lower': -1, 'upper': 1},
+            # }
+        },
+        {
+            'name': 'ecoli',
+            'model': '../data/iECW_1372.xml',  # Path to E. coli model file
+            'flux_id_map': {
+                'glucose': 'EX_glc__D_e',
+                'acetate': 'EX_ac_e',       # Exchange reaction ID for acetate
+                "Maltose": "EX_malt_e"
+            },
+            'kinetic_params': {
+                'glucose': (0.1, 1.0)  #(0.4, 5),  # Km, Vmax for glucose
+                # 'acetate': (0.1, 1.0),  #(0.4, 5),  # Km, Vmax for glucose
+            },
+            'fixed_bounds': {
+                'EX_o2_e': {'lower': -2},  # Setting fixed bounds for E. coli
+                'ATPM': {'lower': 1, 'upper': 1}  # Setting fixed bounds for E. coli
+            #         'EX_fe3dhbzs_e': (0, 10)  # Setting fixed bounds for E. coli
+            }
         }
-    }
+    ]
+    
 }
 
-def run_comets(comets_config, diffusion_field_params, spatial_dfba_params, initial_state_config):
 
-    # create the two processes
+# Initial state configuration
+initial_field_config = {
+    'random': {
+        'glucose': 200.0,
+        'Maltose': 200.0,
+    }}
+initial_species_config = {
+    'random': {
+        'species': {
+            'ecoli': 0.5,
+            'Thermotoga': 0.5
+        }}}
+
+def run_comets(comets_config, diffusion_field_params, spatial_dfba_params, initial_field_config, initial_species_config):
+    # Create the two processes
     diffusion_field = DiffusionField(parameters=diffusion_field_params)
     spatial_dfba = SpatialDFBA(parameters=spatial_dfba_params)
-
-    # set the initial state for diffusion field
-    initial_state_diffusion_field = diffusion_field.initial_state({'random': 1.0})
-
-    # set the initial state for spatial dfba
-    initial_state_spatial_dfba = spatial_dfba.initial_state(initial_state_config)
-
+    
+    # Set the initial state for diffusion field
+    #initial_state_diffusion_field = diffusion_field.initial_state({'random': 1.0})
+    initial_state_diffusion_field = diffusion_field.initial_state(initial_field_config)
+    
+    # Set the initial state for spatial dfba
+    initial_state_spatial_dfba = spatial_dfba.initial_state(initial_field_config)
+    initial_species = spatial_dfba.initial_state(initial_species_config)
+    
+    # Merge into initial state
+    initial_state_spatial_dfba['fields']['glucose'][0, 0] = 0.0  # Set glucose to zero in one bin to see the effect
+    
     # Merge the initial states
     initial_state = {
         'fields': initial_state_diffusion_field['fields'],
-        'species': initial_state_spatial_dfba['species'],
+        'species': initial_species['species'],
     }
-
-    # make the composite simulation and run it
+    
+    # Make the composite simulation and run it
     sim = Engine(
         processes={
             'diffusion_process': diffusion_field,
@@ -126,29 +149,77 @@ def run_comets(comets_config, diffusion_field_params, spatial_dfba_params, initi
         },
         initial_state=initial_state
     )
+    
+    # Run the simulation
     sim.update(comets_config['total_time'])
-
-    # retrieve the results and plot them
+    
+    # Retrieve the results
     data = sim.emitter.get_timeseries()
-    desired_time_points = [1, 3, comets_config['total_time'] - 1]
+    
+    return data
 
-    plot_objective_flux(
-        data,
-        time_points=desired_time_points,
-        species_names=[species['name'] for species in species_info],
-        out_dir='./out',
-        filename='Comets_objective_flux_plot'
-    )
+# def run_comets(comets_config, diffusion_field_params, spatial_dfba_params, initial_state_config):
 
-    plot_fields_temporal(
-        data['fields'],
-        desired_time_points=desired_time_points,
-        actual_time_points=data['time'],
-        plot_fields=["glucose", "oxygen", "ecoli", "Alteromonas"],
-        molecule_colormaps={"glucose": "Blues", "oxygen": "Greens", "ecoli": "Purples", "Alteromonas": "Oranges"},
-        filename='comets_fields',
-    )
+#     # create the two processes
+#     diffusion_field = DiffusionField(parameters=diffusion_field_params)
+#     spatial_dfba = SpatialDFBA(parameters=spatial_dfba_params)
+
+#     # set the initial state for diffusion field
+#     initial_state_diffusion_field = diffusion_field.initial_state({'random': 1.0})
+
+#     # set the initial state for spatial dfba
+#     initial_state_spatial_dfba = spatial_dfba.initial_state(initial_state_config)
+
+#     # Merge the initial states
+#     initial_state = {
+#         'fields': initial_state_diffusion_field['fields'],
+#         'species': initial_state_spatial_dfba['species'],
+#     }
+
+#     # make the composite simulation and run it
+#     sim = Engine(
+#         processes={
+#             'diffusion_process': diffusion_field,
+#             'fba_process': spatial_dfba
+#         },
+#         topology={
+#             'diffusion_process': {
+#                 'fields': ('fields',),
+#                 'species': ('species',),
+#                 'dimensions': ('dimensions',),
+#             },
+#             'fba_process': {
+#                 'fields': ('fields',),
+#                 'species': ('species',),
+#                 'exchange_fluxes': ('exchange_fluxes',),
+#                 'dimensions': ('dimensions',),
+#             }
+#         },
+#         initial_state=initial_state
+#     )
+#     sim.update(comets_config['total_time'])
+
+#     # retrieve the results and plot them
+#     data = sim.emitter.get_timeseries()
+#     desired_time_points = [1, 3, comets_config['total_time'] - 1]
+
+#     plot_objective_flux(
+#         data,
+#         time_points=desired_time_points,
+#         species_names=[species['name'] for species in species_info],
+#         out_dir='./out',
+#         filename='Comets_objective_flux_plot'
+#     )
+
+#     plot_fields_temporal(
+#         data['fields'],
+#         desired_time_points=desired_time_points,
+#         actual_time_points=data['time'],
+#         plot_fields=["glucose", "oxygen", "ecoli", "Alteromonas"],
+#         molecule_colormaps={"glucose": "Blues", "oxygen": "Greens", "ecoli": "Purples", "Alteromonas": "Oranges"},
+#         filename='comets_fields',
+#     )
 
 
 if __name__ == '__main__':
-    run_comets(comets_config, diffusion_field_params, spatial_dfba_params, initial_state_config)
+    run_comets(comets_config, diffusion_field_params, spatial_dfba_params, initial_field_config, initial_species_config)
